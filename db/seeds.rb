4c046e49f48d1fa40c271d68993e62d08425ec87
aoihanabi <abii98cm@gmail.com>
2020-08-13 13:59:36 -0600

    From Windows: Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |b|
+  Blog.create!(
+    title: "My Blog Post #{b}"
+    body: "Sed ut perspiciatis unde omnis iste natus 
+    rror sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
+    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem 
+    quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur"
+  )
+end
+puts "10 Blogs created."
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}"
+    percent_utilized: 15
+  )
+end
+
+puts "5 Blogs created."
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio_item}",
+    subtitle: "My great service",
+    body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
+    voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end 
+
+puts "10 Blogs created."
