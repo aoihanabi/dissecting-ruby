3721756bd26a5c2c47bca66f1dc025a7774e8856
aoihanabi <abii98cm@gmail.com>
2020-08-14 11:15:44 -0600

    Implmemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,7 +7,7 @@ class PortfoliosController < ApplicationController
   def new
     @portf_items = Portfolio.new
   end
-
+  #Creates de item into the DB
   def create 
     @portf_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -18,6 +18,24 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  #Renders the view for the specified item and puts all
+  #the info inside the fields to be edited
+  def edit
+    @portf_items = Portfolio.find(params[:id]);
+
+  end
+  #Writes de changes into the DB
+  def update 
+    @portf_items = Portfolio.find(params[:id]);
+    
+    respond_to do |format|
+      if @portf_items.update(params.require(:portfolio).permit(:title,:subtitle,:body))
+        format.html { redirect_to portfolios_path, notice: "Record successfully updated" }
+      else
+        format.html {render :edit }
+      end
   end
 
 end
