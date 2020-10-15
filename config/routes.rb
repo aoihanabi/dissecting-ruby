35c1af47612b7a8ca11e76690dfab993852d432c
aoihanabi <aoihanabi@DESKTOP-EL1C1HH.localdomain>
2020-10-15 11:18:21 -0600

    Atempt to implement action cable for comments following the tutorial (not working)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do 
     put :sort, on: :collection
@@ -15,6 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #ROOT will declare what's the root of the application
