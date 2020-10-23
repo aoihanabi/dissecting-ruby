3044b689312ca15708558689522f1dcca7a3dbe9
aoihanabi <aoihanabi@DESKTOP-EL1C1HH.localdomain>
2020-10-23 16:39:34 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "test@test.com",
+  password: "123456",
+  password_confirmation: "123456",
+  name: "Admin User",
+  roles: "site_admin"
+)
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "123456",
+  password_confirmation: "123456",
+  name: "Regular User"
+)
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
@@ -27,7 +44,7 @@ puts "5 Blogs created."
 5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
-    percent_utilized: 15
+    percent_utilized: 25
   )
 end
 puts "5 Skills created."
