bb0e6269300b02b728b7420c7a791c216778e1bb
aoihanabi <aoihanabi@DESKTOP-EL1C1HH.localdomain>
2020-10-26 14:15:04 -0600

    Implemented the typed.js library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ require("packs/html5.sortable")
 require("@nathanvda/cocoon")
 require("gritter/js/jquery.gritter.js")
 require("packs/portfolios.coffee")
+require("packs/typed-implementation");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/typed-implementation.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+jQuery(function(){
+    alert('HASHASHAS');
+    var typed = new Typed('.element', {
+        strings: ["This is a longer first sentence.", "Second sentence."],
+        typeSpeed: 30
+    });
+});
