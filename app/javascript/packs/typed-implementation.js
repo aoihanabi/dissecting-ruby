1a274e782d05bbb315731167e3ce13500c3eded1
aoihanabi <aoihanabi@DESKTOP-EL1C1HH.localdomain>
2020-11-03 13:34:16 -0600

    Added links to home page, a quote on the home page, skills in about-me page

--- app/javascript/packs/typed-implementation.js
+++ app/javascript/packs/typed-implementation.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 jQuery(function(){
     var typed = new Typed('.element', {
-        strings: ["This is a longer first sentence.", "Second sentence."],
+        strings: ["Software development is a tool to simplify and enhance.", "My goal is to align with your purpose and apply it to your needs so that it'll complement and boost your work."],
         typeSpeed: 30
     });
 });
