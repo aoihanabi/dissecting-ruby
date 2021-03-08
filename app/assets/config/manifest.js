dd06944d54f66970f0f05e058e708f950e4324d6
aoihanabi <aoihanabi@DESKTOP-EL1C1HH.localdomain>
2021-03-08 16:08:18 -0600

    General changes to add my information and style to the website. Changed background, my information and some colors.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link HardDrivePhotojpeg.mp4
 //= link BlackLines.mp4
+//= link plant.mp4
+//= link water.mp4
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolios/:id', to: 'portfolios#destroy', as: 'delete_portfolio'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :blogs do
